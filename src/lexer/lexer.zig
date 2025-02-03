3ab7258563dd273f12d0e605c3de649d66c0fd9f
Sep <132171290+septechx@users.noreply.github.com>
2025-02-03 14:39:37 +0100

    Update lexer.zig

--- src/lexer/lexer.zig
+++ src/lexer/lexer.zig
@@ -90,6 +90,7 @@ pub fn Tokenize(allocator: std.mem.Allocator, source: []const u8) !std.ArrayList
         .{ .regex = mvzr.compile("\\{").?, .handler = defaultHandler(.OPEN_CURLY, "{") },
         .{ .regex = mvzr.compile("\\}").?, .handler = defaultHandler(.CLOSE_CURLY, "}") },
         .{ .regex = mvzr.compile("[a-zA-Z0-9$_-][a-zA-Z0-9$_-]*").?, .handler = symbolHandler() }, // Handle text, templates, attributes and tags
+        .{ .regex = mvzr.compile("<!--*"), .handler = skipHandler() },
     };
 
     var lex = Lexer.init(allocator, source, &patterns);
