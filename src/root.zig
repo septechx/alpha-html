c6a474609067ad6a33c5987bc86d53a8b399f3bb
septech <septechx@proton.me>
2025-02-05 20:59:01 +0100

    Add root library file, add first real test

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,94 @@
 const std = @import("std");
+const lexer = @import("lexer/lexer.zig");
+const ast = @import("ast/ast.zig");
+const parser = @import("parser/parser.zig");
+const writer = @import("writer/writer.zig");
 
-pub const Html = struct {};
+pub const Html = struct {
+    allocator: std.mem.Allocator,
+    ast: ?ast.BlockStmt,
+    locked: ?ast.LockedBlockStmt,
+    written: ?std.ArrayList(u8),
+
+    pub const WriteOptions = struct {
+        minify: bool,
+    };
+
+    const Self = *Html;
+
+    pub fn init(allocator: std.mem.Allocator) Html {
+        return .{
+            .allocator = allocator,
+            .ast = null,
+            .locked = null,
+            .written = null,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        if (self.ast) |tree| {
+            tree.deinit(self.allocator);
+        }
+        if (self.written) |written| {
+            written.deinit();
+        }
+        if (self.locked) |locked| {
+            locked.deinit(self.allocator);
+        }
+    }
+
+    pub fn parse(self: Self, html: []const u8) !void {
+        var tokens = try lexer.Tokenize(self.allocator, html);
+        defer tokens.deinit();
+
+        self.ast = try parser.Parse(self.allocator, tokens);
+    }
+
+    pub fn lock(self: Self) !void {
+        if (self.ast) |tree| {
+            const locked = try tree.lock(self.allocator);
+            self.locked = locked.block;
+        } else {
+            @panic("called lock() withought an ast, call parse() first");
+        }
+    }
+
+    pub fn write(self: Self, options: WriteOptions) ![]const u8 {
+        if (self.locked) |locked| {
+            self.written = try writer.write(self.allocator, &ast.LockedStmt{ .block = locked }, !options.minify, true);
+            return self.written.?.items;
+        }
+        try self.lock();
+        return self.write(options);
+    }
+
+    pub fn getAst(self: Self) ast.BlockStmt {
+        if (self.ast) |tree| {
+            return tree;
+        } else {
+            @panic("called getAst() withought an ast, call parse() first");
+        }
+    }
+
+    pub fn writeAst(self: Self, tree: ast.BlockStmt) void {
+        self.ast = tree;
+    }
+};
 
 test {
     std.testing.refAllDecls(@This());
+}
+
+test "parse and write simple div with text" {
+    const allocator = std.testing.allocator;
+
+    var html = Html.init(allocator);
+    defer html.deinit();
+
+    const input = "<div>Hello world</div>";
+    try html.parse(input);
+
+    const output = try html.write(.{ .minify = true });
+
+    try std.testing.expect(std.mem.eql(u8, output, "<!DOCTYPE html><div>Helloworld</div>"));
 }
