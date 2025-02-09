e008ff8e7a6775e84cbf7278b8b9e48ea77bb703
septech <septechx@proton.me>
2025-02-09 23:27:03 +0100

    Remove main.zig, add installing guide

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # α-html
+
+## Installing
+
+First run:
+
+```sh
+zig fetch --save git+https://github.com/septech/alpha-html#v0.1.0
+```
+
+Then add this to your `build.zig` before `b.installArtifact(exe)`:
+
+```zig
+const @"alpha-html" = b.dependency("alpha-html", .{
+    .target = target,
+    .optimize = optimize,
+});
+
+exe.root_module.addImport("alpha-html", @"alpha-html".module("alpha-html"));
+
+```
--- build.zig
+++ build.zig
@@ -15,13 +15,6 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "test-file",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const lib = b.addStaticLibrary(.{
         .name = "alpha-html",
         // In this case the main source file is merely a path, however, in more
@@ -37,36 +30,11 @@ pub fn build(b: *std.Build) void {
     });
 
     lib.root_module.addImport("mvzr", mvzr.module("mvzr"));
-    exe.root_module.addImport("mvzr", mvzr.module("mvzr"));
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(exe);
     b.installArtifact(lib);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -76,22 +44,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     lib_unit_tests.root_module.addImport("mvzr", mvzr.module("mvzr"));
-    exe_unit_tests.root_module.addImport("mvzr", mvzr.module("mvzr"));
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
